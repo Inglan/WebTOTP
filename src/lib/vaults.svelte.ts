199a62a7205f1014315f7a29d7c76c55e59cc9d2
Ingo Wolf <me@ingowolf.au>
2025-06-09 10:35:13 +1000

    add create function

--- src/lib/vaults.svelte.ts
+++ src/lib/vaults.svelte.ts
@@ -2,3 +2,7 @@ import { db } from "./db";
 import { liveQuery } from "dexie";
 
 export let vaults = liveQuery(() => db.vaults.toArray());
+
+export function create(name: string) {
+  db.vaults.add({ name, id: crypto.randomUUID() });
+}
