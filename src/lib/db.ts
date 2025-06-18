14123c2e8c650a3f4010f489f3fe1dbb845c79ed
Ingo Wolf <me@ingowolf.au>
2025-06-18 17:48:38 +1000

    remove comments

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,13 +2,13 @@ import Dexie from "dexie";
 import { type EntityTable } from "dexie";
 
 interface Vault {
-  id: string; // UUID or short id
+  id: string;
   name: string;
 }
 
 interface VaultItem {
-  id: string; // unique within vault
-  vaultId: string; // foreign key to Vault
+  id: string;
+  vaultId: string;
   encryptedData: Uint8Array;
   iv: Uint8Array;
 }
