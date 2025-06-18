58f05ea242027899919307b376c7700a5a4958ca
Ingo Wolf <me@ingowolf.au>
2025-06-18 17:48:23 +1000

    remove vaultkey

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,28 +6,6 @@ interface Vault {
   name: string;
 }
 
-interface VaultKey {
-  vaultId: string; // foreign key to Vault
-
-  // Encrypted using password-derived key
-  encryptedByPassword: {
-    ciphertext: Uint8Array;
-    iv: Uint8Array;
-    salt: Uint8Array;
-    iterations: number;
-  };
-
-  // Encrypted using passkey-derived key
-  encryptedByPasskey: {
-    ciphertext: Uint8Array;
-    iv: Uint8Array;
-    prfContext: Uint8Array;
-  };
-
-  // Optional key checksum
-  checksum?: Uint8Array;
-}
-
 interface VaultItem {
   id: string; // unique within vault
   vaultId: string; // foreign key to Vault
@@ -37,15 +15,13 @@ interface VaultItem {
 
 const db = new Dexie("WebTOTPDatabase") as Dexie & {
   vaults: EntityTable<Vault, "id">;
-  vaultKeys: EntityTable<VaultKey, "vaultId">;
   vaultItems: EntityTable<VaultItem, "id">;
 };
 
 db.version(1).stores({
   vaults: "id, name",
-  vaultKeys: "vaultId",
   vaultItems: "id, vaultId",
 });
 
-export type { Vault, VaultKey, VaultItem };
+export type { Vault, VaultItem };
 export { db };
