dfbe838d6d714d9915437c9af6b20f82c648314d
Ingo Wolf <me@ingowolf.au>
2025-06-18 18:04:40 +1000

    add encryptedmasterkey table

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,20 +6,31 @@ interface Vault {
   name: string;
 }
 
+interface EncryptedMasterKey {
+  id: string;
+  vaultId: string;
+  encryptedKey: Uint8Array;
+  salt: Uint8Array;
+  iv: Uint8Array;
+}
+
 interface VaultItem {
   id: string;
   vaultId: string;
   encryptedData: Uint8Array;
+  iv: Uint8Array;
 }
 
 const db = new Dexie("WebTOTPDatabase") as Dexie & {
   vaults: EntityTable<Vault, "id">;
   vaultItems: EntityTable<VaultItem, "id">;
+  encryptedMasterKeys: EntityTable<EncryptedMasterKey, "id">;
 };
 
 db.version(1).stores({
   vaults: "id, name",
   vaultItems: "id, vaultId",
+  encryptedMasterKeys: "id, vaultId",
 });
 
 export type { Vault, VaultItem };
